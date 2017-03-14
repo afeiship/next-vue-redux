55c7656264502277c269b2b5792d40d80c8e4e55
afeiship <1290657123@qq.com>
2017-03-14 22:58:08 +0800

    intial

---
+++ src/redux-boot.js
@@ -0,0 +1,57 @@
+var Vue = require('vue');
+var nx = require('next-js-core2');
+var createStore=require('redux').createStore;
+var bindActionCreators=require('redux').bindActionCreators;
+var applyMiddleware = require('redux').applyMiddleware;
+var ReduxThunk = require('redux-thunk').default;
+var States = require('./redux-states');
+var Actions = require('./redux-actions');
+var Reducers = require('./redux-reducers');
+
+var ReduxBoot = nx.declare({
+  statics:{
+    run:function(inApp,inAppId){
+      return new ReduxBoot(inApp,inAppId);
+    }
+  },
+  methods:{
+    init(inApp,inAppId){
+      this._app = inApp;
+      this._store = createStore(
+        this.reducers.bind(this),
+        applyMiddleware(ReduxThunk)
+      );
+      this._container ='#'+inAppId;
+      this.subscribe();
+      this.renderTo();
+    },
+    reducers:function (inState,inAction) {
+      var initialState = this._app.initialState();
+      return Reducers( inState || initialState ,inAction);
+    },
+    subscribe: function() {
+      this._store.subscribe(this.renderTo.bind(this));
+    },
+    renderTo: function() {
+      new Vue({
+        router,
+        render:function(createElement){
+          return createElement(App,{
+              store: this._store,
+              getState:this._store.getState.bind(this),
+              dispatch:this._store.dispatch.bind(this),
+              actions:bindActionCreators(Actions, this._store.dispatch),
+              update: States.getUpdate.bind(this,this._store),
+              root: States.getRoot.bind(this,this._store),
+              memory: States.getMemory.bind(this,this._store),
+              request: States.getRequest.bind(this,this._store),
+              local: States.getLocal.bind(this),
+              session: States.getSession.bind(this),
+          });
+        }
+      }).$mount(this._container);
+    }
+  }
+});
+
+module.exports = ReduxBoot;
